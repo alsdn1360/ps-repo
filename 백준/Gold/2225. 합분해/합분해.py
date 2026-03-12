eab1054be77958f889a31044ed483dd5a081064a
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-03-12 14:44:22 +0900

    [Gold V] Title: 합분해, Time: 48 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -4,8 +4,6 @@ MOD = 1_000_000_000
 # main
 n, k = map(int, input().split())
 
-answer = 0
-
 dp = [[0 for _ in range(n + 1)] for _ in range(k + 1)]
 
 # 숫자 1 ~ k개로 0을 만드는 방법
@@ -18,8 +16,6 @@ for j in range(1, n + 1):
 
 for i in range(1, k + 1):
     for j in range(1, n + 1):
-        dp[i][j] = dp[i - 1][j] + dp[i][j - 1]
+        dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD
 
-answer = dp[k][n] % MOD
-
-print(answer)
+print(dp[k][n])
