24a89542c72a647e4c38f854be650bc4c21c1dfd
강동우 <110466553+alsdn1360@users.noreply.github.com>
2026-03-12 14:45:11 +0900

    [Gold V] Title: 합분해, Time: 48 ms, Memory: 34456 KB -BaekjoonHub

--- 백준/Gold/2225. 합분해/합분해.py
+++ 백준/Gold/2225. 합분해/합분해.py
@@ -14,7 +14,7 @@ for i in range(1, k + 1):
 for j in range(1, n + 1):
     dp[1][j] = 1
 
-for i in range(2, k + 1):
+for i in range(1, k + 1):
     for j in range(1, n + 1):
         dp[i][j] = (dp[i - 1][j] + dp[i][j - 1]) % MOD
 
